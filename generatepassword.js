af72672b52e70c79cc684b689e298728d385b064
Praduman sharma <123444443+praduman1212@users.noreply.github.com>
2023-08-22 17:46:39 +0530

    Add files via upload

---
+++ generatepassword.js
@@ -0,0 +1,15 @@
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
+    let password = " ";
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+    }
+    
+    return password;
+}
+
+const passwordLength = 12; // Change this to your desired password length
+const generatedPassword = generatePassword(passwordLength);
+console.log("Generated Password:", generatedPassword); 
